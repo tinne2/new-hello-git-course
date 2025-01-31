8601222820a72e5af79349074f075c28b02f37fe
Tinne Dey <118190717+tinne2@users.noreply.github.com>
2025-01-31 15:53:06 +0200

    Update mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,6 +1,4 @@
-def debug_print(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+        print("{}: {}".format(key, value))
 
 
 def mergesort(array):
@@ -39,15 +37,7 @@ def merge(left, right):
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-    input_list = input_str.split(",")
-    value_list = []
-    
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
-            quit(1)  # Make sure this line is properly indented within the try-except block.
+    quit(1)
 
     debug_print(value_list=value_list)
 
